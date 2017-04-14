572cb03e9f61095f377acf21bf530cae0baec34d
Courtney Labog <labogcourtney@gmail.com>
2017-04-14 13:46:41 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,18 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+
+// ================================
+// START YOUR APP HERE
+// ================================
+
+console.log('hello');
+
+$('section').on('click', function(){
+	console.log('hi');
+});
+
+
